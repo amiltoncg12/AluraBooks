bf3028eb1ea5d79f0f1e7b1cbff0183d691a6183
Josemar amilton silva de almeida <amilton_cg12@hotmail.com>
2024-12-12 19:57:21 -0300

    projeto inicial

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function clicaMenu() {
+    if(itens.style.display == 'none') {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
+    }
+}
+
+function clicaMenu2() {
+    if(itens2.style.display == 'none') {
+        itens2.style.display = 'block';
+    } else {
+        itens2.style.display = 'none';
+    }
+}
